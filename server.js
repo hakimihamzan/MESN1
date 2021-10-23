93a9e73a88868f4fa6385e62c15a33c17758c9df
hakimihamzan <hakimihamzan20@gmail.com>
2021-10-23 16:37:12 +0800

    updated backend

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const port = 3000;
+//odm
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+// thic contains config things
+const { port, mongoURI } = require("./config/config");
+//route path
+const transactionsRoutes = require("./routes/transaction");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+//connecting to db first, using mongoose
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB database is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/api/transactions", transactionsRoutes);
 
 app.get("/", (req, res) => res.send("Hello world"));
 
